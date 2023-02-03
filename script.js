3621c1f6c66935ad822643a72f8aed1ee9ad1229
Samson Orr <171129@student.vansd.org>
2023-02-03 23:43:22 +0000

    better site

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-var myWindow = window.open("", "");
+function theveryback() {
+var myWindow = window.open("", "", "");
 myWindow.document.write("<meta name='viewport' content='width=device-width' /><link href='https://replitcss.tehost.ml/replit.min.css' rel='stylesheet' type='text/css' /><iframe src='https://purple.tehost.ml' frameborder='0' allowfullscreen></iframe>");
+}
