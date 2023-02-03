1ba6c75e30a5d6a42b7313c5fa76e1869d979216
Samson Orr <171129@student.vansd.org>
2023-02-03 20:52:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,2 @@
+var myWindow = window.open("", "");
+myWindow.document.write("<meta name='viewport' content='width=device-width' /><link href='https://replitcss.tehost.ml/replit.min.css' rel='stylesheet' type='text/css' /><iframe src='https://purple.tehost.ml' frameborder='0' allowfullscreen></iframe>");
